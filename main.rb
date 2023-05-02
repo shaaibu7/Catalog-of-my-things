0fa928612586e4237b6bde4ecd70018a301ba02c
Habtamu210 <beleyaha@gmail.com>
2023-05-02 06:13:51 -0700

    Create an App that serve as a  console app entry-point

--- main.rb
+++ main.rb
@@ -1,1 +1,14 @@
+require_relative './app/app'
 
+class Main
+  app = App.new
+
+  key = ''
+
+  until key == '8'
+    app.show_menu
+    key = gets.chomp
+    app.run(key)
+  end
+  puts 'Well come to our Application'
+end
