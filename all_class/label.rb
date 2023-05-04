c69a7f69a3042f22de4bd8bc918797ceebe9df03
Habtamu210 <beleyaha@gmail.com>
2023-05-04 00:53:11 -0700

    Update the label class

--- all_class/label.rb
+++ all_class/label.rb
@@ -1,29 +1,15 @@
 require_relative './item'
 
-class Label
-  attr_accessor :title, :color
-  attr_reader :id, :items
-
-  def initialize(title, color)
-    @id = rand(0..1000)
+class Label < Item
+  def initialize(title, color, _items)
+    super
+    @id = round(0..1000)
     @title = title
     @color = color
     @items = []
   end
 
   def add_item(item)
-    @items << item unless @items.exist?(item)
-    item.label = self
-  end
-
-  def to_json(*arg)
-    {
-      JSON.create_id => self.class.name,
-      'a' => [title, color]
-    }.to_json(*arg)
-  end
-
-  def json_create(object)
-    new(*object['a'])
+    @items << item unless @item.exist?(item)
   end
 end
