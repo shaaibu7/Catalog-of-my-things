40cbfd64c9e676dcfe88daf784d2913c084c1350
Habtamu210 <beleyaha@gmail.com>
2023-05-04 01:20:50 -0700

    Add database for genre classs

--- data/schema.sql
+++ data/schema.sql
@@ -22,3 +22,14 @@ create table music_album(
 	FOREIGN Key(label_id) REFERENCES label(id),
     PRIMARY KEY(id)
 );
+
+CREATE INDEX idx_genre_id ON music_album (genre_id)
+CREATE INDEX idx_author_id ON music_album (author_id)
+CREATE INDEX idx_label_id ON music_album (label_id)
+
+create table genre(
+	id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(100),
+    items TEXT[],
+    PRIMARY KEY(id)
+);
