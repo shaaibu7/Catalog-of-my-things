654d03e95a9802b81f83d241e07f1a0280e556b4
shaaibu <shaaibusuleiman@gamil.com>
2023-05-04 11:34:48 +0100

    Implement game and author class

---
+++ all_class/game.rb
@@ -0,0 +1,15 @@
+require 'date'
+
+class Game < Item
+  attr_reader :multiplayer, :last_played_at
+
+  def initialize(publish_date, multiplayer, last_played_at)
+    super(publish_date)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
+
+  def can_be_archived?
+    super && ( Time.now.year - Date.parse(last_played_at).year) > 2
+  end
+end
